29568a2722917f17ad6c74fa85f8d96ab8d60b2e
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-04-02 23:54:36 +0300

    Initial Commit

---
+++ JustLoginAndTimer/JustLoginAndTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustLoginAndTimerApp.swift
+//  JustLoginAndTimer
+//
+//  Created by ALEKSEY SUSLOV on 02.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct JustLoginAndTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
