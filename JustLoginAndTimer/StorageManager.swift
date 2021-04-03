53488fb381c0991c76cde5bec8d43ab22891ae5e
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-04-04 00:13:07 +0300

    fixed a little mistake

--- JustLoginAndTimer/StorageManager.swift
+++ JustLoginAndTimer/StorageManager.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class StorageManager {
-    static var shared = StorageManager()
+    static let shared = StorageManager()
     
     private let userDefaults = UserDefaults.standard
     
